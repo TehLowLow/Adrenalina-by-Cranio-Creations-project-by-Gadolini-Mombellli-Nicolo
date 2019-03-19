d5a649f748fd637bf084d8ef094067430f2ea519
lollo <lorenzo.gadolini@mail.polimi.it>
2019-03-19 17:10:29 +0100

    First Commit

---
+++ src/main/java/it/polimi/se2019/App.java
@@ -0,0 +1,13 @@
+package it.polimi.se2019;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/it/polimi/se2019/AppTest.java
@@ -0,0 +1,20 @@
+package it.polimi.se2019;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
