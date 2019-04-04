25aa971feb1d1a32c02425e1e38e9146fd3dec43
Dany97 <daniele.nicolo@mail.polimi.it>
2019-04-04 22:35:11 +0200

    controller - classi turn e interaction

---
+++ src/main/java/it/polimi/se2019/Controller/Adrenalina/Interaction.java
@@ -0,0 +1,141 @@
+package it.polimi.se2019.Controller.Adrenalina;
+
+import it.polimi.se2019.Model.*;
+
+import java.util.ArrayList;
+
+public class Interaction {
+    /**
+     * this class contains the methods to manage the links
+     * with all the game's elements, such as drawing, discarding...
+     */
+
+    /*
+    ----------------------METHODS--------------------------------
+     */
+
+
+    /**
+     * this method is used to allow the player to draw a powerUp from
+     * the board
+     * @param player is who has to draw the powerUp
+     * @param board is "Adrenalina" main board
+     */
+
+    public void drawPowerUp(Player player, Board board){}
+
+
+    /**
+     * this method is used to allow the player to discard a powerUp
+     * @param player is who has to discard the powerUp
+     * @param board is "Adrenalina" main board
+     */
+
+    public void discardPowerUp(Player player, Board board){}
+
+
+    /**
+     * this method draws a weapon from the deck and places
+     * it on the board, making it reachable from the spawn
+     * cell
+     * @param board is "Adrenalina" main board
+     */
+
+    public void placeWeapon(Board board){}
+
+
+    /**
+     * this method is used to recharge a unloaded weapon
+     * @param weapon is the weapon to reload
+     */
+
+    public void reloadWeapon(Weapon weapon){}
+
+
+    /**
+     * this method allows the player to get a weapon
+     * from the available weapons in the spawn point
+     * @param player is the player who gets the weapon
+     * @param weapon is the selected weapon
+     */
+
+    public void getWeapon(Player player, Weapon weapon){}
+
+    /**
+     * this method replaces a player's weapon with a new
+     * weapon taken from the spawn cell.
+     * the old player's weapon is placed instead of the
+     * new one.
+     * @param oldWeapon is the weapon that is placed on the board
+     * @param newWeapon is the weapon taken by the player
+     */
+
+    public void switchWeapon(Weapon oldWeapon, Weapon newWeapon){}
+
+    /**
+     * this method reloads on the board the Loots that
+     * have been picked up during the turn
+     * @param board is the main board
+     */
+
+    public void placeLoot(Board board){}
+
+
+    /**
+     * this method discards a loot picked up in the turn
+     * @param cell is the cell where the loot has been picked up
+     * @param board is the main board
+     */
+
+    public void discardLoot(LootCell cell, Board board){}
+
+
+    /**
+     * this method manages a player's rybamount payment
+     * @param player is who has to pay the cubes
+     * @param rybamount are the cubes to pay
+     */
+
+    public void pay(Player player, Rybamount rybamount){}
+
+    /**
+     * this method replaces a skull on the MortalBlow track
+     * with the killer's Token
+     * @param board is the main board
+     * @param killer is the player who has to place his Token
+     * @param victim is the killed player
+     * @param overkill is a boolean that is '1' in case of overkill,
+     *                 '0' otherwise
+     */
+
+
+    public void claimSkull(Board board, Player killer, Player victim, boolean overkill){}
+
+    /**
+     * this method turns the playerboard at the beginning
+     * of the FinalFrenzy mode
+     * @param player is the player linked to the playerboard
+     *               to turn
+     */
+
+    public void turnPlayerboard(Player player){}
+
+    /**
+     * this method turns the action tile for the FinalFrenzy mode
+     * @param player is linked to the tile to turn
+     */
+
+    public void turnActionTile(Player player){}
+
+
+    /**
+     * this methos gives to a player the cubes corresponding
+     * to the picked up loot
+     * @param player is the player who receives the cubes
+     */
+
+    public void giveRybamount(Player player){}
+
+
+
+}
---
+++ src/main/java/it/polimi/se2019/Controller/Adrenalina/Turn.java
@@ -0,0 +1,97 @@
+package it.polimi.se2019.Controller.Adrenalina;
+
+import it.polimi.se2019.Model.Player;
+import it.polimi.se2019.Model.Powerup;
+
+import java.util.ArrayList;
+
+public class Turn {
+    /**
+     * This class represents the player's turn in the match
+     */
+
+    /*
+    ------------FIELDS-------------------------
+     */
+
+    /**
+     * drewPowerUp is used to save the PowerUp that the player has drawn
+     */
+
+    private ArrayList<Powerup> drewPowerUp;
+
+
+
+    /*
+    -----------------METHODS--------------------
+     */
+
+
+    /*
+    ------------------GETTERS--------------------
+     */
+
+    public ArrayList<Powerup> getDrewPowerUp() {
+        return drewPowerUp;
+    }
+
+    /*
+    -----------------SETTERS-----------------------
+     */
+
+    public void setDrewPowerUp(ArrayList<Powerup> drewPowerUp) {
+        this.drewPowerUp = drewPowerUp;
+    }
+
+    /*
+    ----------------OTHER METHODS-------------------
+    */
+
+
+    /**
+     *
+     * @param time is the max time usable for the turn.
+     *             When it's over, the turn is skipped.
+     */
+    public void timer(int time){}
+
+
+    /** this method allows the player to play the first turn
+     *
+     * @param player is the player who has to play the first turn
+     */
+    public void first(Player player){}
+
+
+    /**
+     * this method allows the player to play a standard turn
+     * @param player is the player who has to play the turn
+     */
+    public void standard(Player player){}
+
+
+    /**
+     * allows the player to spawn in the map for the first time
+     * @param player is the player that has to spawn
+     */
+
+    public void firstSpawn(Player player){}
+
+
+    /**
+     * it allows the player to respawn in the map after his death
+     * @param player is the player that has to respawn
+     */
+
+    public void respawn(Player player){}
+
+
+    /**
+     * it allows the player to play his FinalFrenzy turn
+     * @param player is who has to play the FinalFrenzy turn
+     */
+
+    public void frenzy(Player player){}
+
+
+}
