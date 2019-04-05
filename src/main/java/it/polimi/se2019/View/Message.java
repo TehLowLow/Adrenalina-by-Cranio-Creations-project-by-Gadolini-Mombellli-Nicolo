8cce53d0a7a3d389cc7b50c962221c220c5e0dbc
lolepls <gioele.mombelli@mail.polimi.it>
2019-04-05 23:02:56 +0200

    Adds View package.

---
+++ src/main/java/it/polimi/se2019/View/Message.java
@@ -0,0 +1,11 @@
+package it.polimi.se2019.View;
+
+public class Message {
+
+    public void title(){
+
+        System.out.println("Benvenuto in Adrenalina!");
+
+    }
+
+}
---
+++ src/main/java/it/polimi/se2019/View/Parser.java
@@ -0,0 +1,13 @@
+package it.polimi.se2019.View;
+
+public class Parser {
+
+    public String parse(){
+        /*
+        Legge ciò che è stato digitato dall'utente e lo restituisce.
+         */
+
+        return new String();
+    }
+
+}
