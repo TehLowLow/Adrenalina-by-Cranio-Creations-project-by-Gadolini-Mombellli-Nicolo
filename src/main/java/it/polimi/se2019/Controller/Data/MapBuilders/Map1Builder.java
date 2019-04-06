1f0cb950174666e259ff9ed85c749d012b8e994b
Dany97 <daniele.nicolo@mail.polimi.it>
2019-04-06 16:59:29 +0200

    Data and Setup

--- src/main/java/it/polimi/se2019/Controller/Data/MapBuilders/Map1Builder.java
+++ src/main/java/it/polimi/se2019/Controller/Data/MapBuilders/Map1Builder.java
@@ -1,4 +1,25 @@
 package it.polimi.se2019.Controller.Data.MapBuilders;
+import it.polimi.se2019.Model.*;
+import it.polimi.se2019.Controller.Data.RoomBuilders.*;
 
 public class Map1Builder {
+
+    //private Map map1 = new Map();
+
+    public Map build(){
+
+        /*
+        ------------------YELLOW ROOM-------------------
+         */
+
+        YellowRoomBuilder yellowRoomBuilder = new YellowRoomBuilder();;
+
+        Room yellowRoom = new Room();
+        yellowRoom = yellowRoomBuilder.build();
+
+
+
+        return map1;
+    }
+
 }
