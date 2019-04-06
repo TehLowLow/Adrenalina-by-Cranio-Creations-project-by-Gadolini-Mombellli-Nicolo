37e9be85d0de8fcc850d80af86635a0b1dd9074e
lolepls <gioele.mombelli@mail.polimi.it>
2019-04-06 16:45:41 +0200

    Adds some code to BoardSetup.

--- src/main/java/it/polimi/se2019/Controller/Setup/BoardSetup.java
+++ src/main/java/it/polimi/se2019/Controller/Setup/BoardSetup.java
@@ -8,8 +8,18 @@ public class BoardSetup {
         Board board = new Board();
 
         WeaponDeckSetup weaponDeckSetup = new WeaponDeckSetup();
-        Map1Setup map1Setup = new Map1Setup();
         board.setWeaponDeck(weaponDeckSetup.build());
+
+        Map1Setup map1Setup = new Map1Setup();
+        board.setMap(Map1Setup.build());
+
+        LootDeckSetup lootDeckSetup = new LootDeckSetup();
+        board.setLootDeck(lootDeckSetup.build());
+
+        PowerUpDeckSetup powerUpDeckSetup = new PowerUpDeckSetup();
+        board.setPowerUpDeck(powerUpDeckSetup.build());
+
+
         return board;
 
     }
