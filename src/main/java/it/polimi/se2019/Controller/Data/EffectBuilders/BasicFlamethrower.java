287ac532bc9c0b7c6b77e9773419bb73fa6c937c
TehLowLow <35232619+TehLowLow@users.noreply.github.com>
2019-04-07 13:34:01 +0200

    Added JDocs for the effect classes.

--- src/main/java/it/polimi/se2019/Controller/Data/EffectBuilders/BasicFlamethrower.java
+++ src/main/java/it/polimi/se2019/Controller/Data/EffectBuilders/BasicFlamethrower.java
@@ -2,13 +2,22 @@ package it.polimi.se2019.Controller.Data.EffectBuilders;
 import it.polimi.se2019.Model.Effect;
 import it.polimi.se2019.Model.Map;
 import it.polimi.se2019.Model.Player;
-
 import java.util.ArrayList;
 
+
+
+/**
+ * Contains the @Override of the methods of Effect.java, and builds the Flamethrower.
+ */
 public class BasicFlamethrower {
 
         private Effect basicFlamethrower = new Effect() {
 
+         /**
+          * Applies the effect of the Flamethrower to the target (or targets).
+          * @param user the Player that wants to apply the effect.
+          * @param target the target of the effect. It can be the user itself.
+          */
 
         @Override
         public void applyEffect(Player user, Player target) {
@@ -16,10 +25,16 @@ public class BasicFlamethrower {
 
         }
 
+            /**
+             * Looks for the target (or targets) of the Flamethrower.
+             * @param user the Player thant wants to use the effect.
+             * @param map the Map where the targets must be searched.
+             * @return
+             */
+
         @Override
         public ArrayList<Player> getTargets(Player user, Map map) {
             return null;
         }
     };
-
 }
--- src/main/java/it/polimi/se2019/Controller/Data/EffectBuilders/BasicHeatSeeker.java
+++ src/main/java/it/polimi/se2019/Controller/Data/EffectBuilders/BasicHeatSeeker.java
@@ -1,18 +1,35 @@
 package it.polimi.se2019.Controller.Data.EffectBuilders;
-
 import it.polimi.se2019.Model.Effect;
 import it.polimi.se2019.Model.Map;
 import it.polimi.se2019.Model.Player;
-
 import java.util.ArrayList;
 
+
+
+/**
+ * Contains the @Override of the methods of Effect.java, and builds the Heatseeker.
+ */
 public class BasicHeatSeeker {
 
     private Effect basicHeatSeeker = new Effect() {
+
+        /**
+         * Applies the effect of the Heatseeker to the target.
+         * @param user the Player that wants to apply the effect.
+         * @param target the target of the effect. It can be the user itself.
+         */
+
         @Override
         public void applyEffect(Player user, Player target) {
 
         }
+
+        /**
+         * Looks for the target of the Heatseeker.
+         * @param user the Player thant wants to use the effect.
+         * @param map the Map where the targets must be searched.
+         * @return
+         */
 
         @Override
         public ArrayList<Player> getTargets(Player user, Map map) {
