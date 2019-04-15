2a4758934d0ea5c52b8b2a5a97962e6635fa434a
TehLowLow <35232619+TehLowLow@users.noreply.github.com>
2019-04-15 23:10:38 +0200

    Reverted from Gson Added code for WeaponBuilders

---
+++ src/main/java/it/polimi/se2019/Controller/Data/WeaponBuilders/BlueWeapons/LockRifleBuilder.java
@@ -0,0 +1,38 @@
+package it.polimi.se2019.Controller.Data.WeaponBuilders.BlueWeapons;
+
+
+import it.polimi.se2019.Model.Effect;
+import it.polimi.se2019.Model.Rybamount;
+import it.polimi.se2019.Model.Weapon;
+
+public class LockRifleBuilder {
+
+    private Weapon lRifle = new Weapon();
+
+    public Weapon build() {
+
+        Rybamount price = new Rybamount();
+
+        price.setBlueCubes(1);
+        price.setYellowCubes(0);
+        price.setRedCubes(0);
+
+        Rybamount recharge = new Rybamount();
+
+        recharge.setBlueCubes(2);
+        recharge.setRedCubes(0);
+        recharge.setYellowCubes(0);
+
+        lRifle.setName("Distruttore");
+        lRifle.setPrice(price);
+        lRifle.setRechargeCost(recharge);
+        lRifle.setLoaded(true);
+        lRifle.setBaseEffect(null);
+        lRifle.setAlternativeEffect(null);
+        lRifle.setOptionalEffect(null);
+
+
+        return lRifle;
+    }
+
+}
--- src/main/java/it/polimi/se2019/Controller/Data/WeaponBuilders/RedWeapons/FlamethrowerBuilder.java
+++ src/main/java/it/polimi/se2019/Controller/Data/WeaponBuilders/RedWeapons/FlamethrowerBuilder.java
@@ -63,7 +63,7 @@ public class FlamethrowerBuilder {
 
     }
 
-    //Gson test
+
 
 
 
