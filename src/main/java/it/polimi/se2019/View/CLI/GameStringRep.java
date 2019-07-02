43a74ca8f9439f58b24858df07c2d800478def7b
lolepls <gioele.mombelli@mail.polimi.it>
2019-07-02 16:11:04 +0200

    Last bugfixes.

--- src/main/java/it/polimi/se2019/View/CLI/GameStringRep.java
+++ src/main/java/it/polimi/se2019/View/CLI/GameStringRep.java
@@ -324,11 +324,21 @@ codificata e inviata.
 
         int damageNumber = player.getPlayerboard().getDamage().size();
 
+        if(damageNumber>12){
+            damageNumber = 12;
+        }
+
         GUIData = GUIData + damageNumber + '@';
+
+        int i = 0;
 
         for(Token damage : player.getPlayerboard().getDamage()){
 
+            if (i > 11) {
+                break;
+            }
             GUIData = GUIData + damage.getChampionName() + '@';
+            i++;
 
         }
 
