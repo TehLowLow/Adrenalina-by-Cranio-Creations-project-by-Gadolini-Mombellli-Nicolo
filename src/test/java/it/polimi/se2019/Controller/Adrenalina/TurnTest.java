1ff8679878628b6f5af4e1c7b2bad979b4bb0796
Dany97 <daniele.nicolo@mail.polimi.it>
2019-07-03 18:32:34 +0200

    testing

--- src/test/java/it/polimi/se2019/Controller/Adrenalina/TurnTest.java
+++ src/test/java/it/polimi/se2019/Controller/Adrenalina/TurnTest.java
@@ -6,6 +6,7 @@ import it.polimi.se2019.Controller.Data.PowerUpBuilder.BlueTagbackGrenadeBuilder
 import it.polimi.se2019.Model.Board;
 import it.polimi.se2019.Model.Player;
 import it.polimi.se2019.Model.Powerup;
+import it.polimi.se2019.Model.Token;
 import it.polimi.se2019.Network.Server;
 import it.polimi.se2019.Network.TestBot;
 import org.junit.Before;
@@ -13,6 +14,7 @@ import org.junit.Test;
 
 import java.util.concurrent.CopyOnWriteArrayList;
 
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
 public class TurnTest {
@@ -34,6 +36,7 @@ public class TurnTest {
         answers.add("muovi");
         answers.add("1");
         answers.add("0");
+        answers.add("0");
 
         TestBot.initAnswers(answers);
 
@@ -51,10 +54,43 @@ public class TurnTest {
 
         player.getPlayerboard().getPowerups().add(tagBack);
 
+        Player killed = Server.connectedPlayers.get(1);
+
+        for (int i=0; i<12; i++) {
+
+            Token token = new Token();
+            token.setChampionName("champion0");
+            killed.getPlayerboard().getDamage().add(token);
+        }
 
         t.standard(player, false);
 
         assertTrue(player.getPosition().equals(Board.getMap().getBlueRoom().getCells().get(0)));
+        assertTrue(killed.getPlayerboard().getDamage().size()==0);
+
+
+    }
+
+
+    @Test
+    public void firstSpawnNoTerminator(){
+
+        CopyOnWriteArrayList<String> answers = new CopyOnWriteArrayList<>();
+        answers.add("0");
+        answers.add("muovi");
+        answers.add("1");
+        answers.add("0");
+        answers.add("muovi");
+        answers.add("1");
+        answers.add("0");
+        TestBot.initAnswers(answers);
+
+        Player player = Server.connectedPlayers.get(0);
+        Turn t = new Turn();
+        t.first(player);
+
+
+        assertFalse(player.getPosition().equals(Board.getLimbo()));
 
 
     }
